e8173246fe48e96b2b2d5eff685cd44379231fad
mika tiihonen <koneautomaatio@hotmail.com>
2021-12-28 22:36:39 +0200

    randomColors added

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ const candyColors = [
   'yellow'
 ]
 
-function App() {
+const App = () => {
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)
+    }
+  }
+
+  createBoard()
+
   return (
     <div></div>
   )
